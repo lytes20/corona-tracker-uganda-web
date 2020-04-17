0a05cf47228d136f016eb719d8fe74730164104e
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-04-17 20:23:01 +0300

    Divide home into two, main and side

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
+
+import './assets/styles/home.scss';
+
 import Header from './components/shared/Header';
 import DateTime from './components/client/DateTime';
 import Cases from './components/client/Cases';
-import Past14DaysChart from './components/client/Past14DaysChart'
+import Past14DaysChart from './components/client/Past14DaysChart';
 
 function App() {
   return (
     <div>
       <Header />
-      <DateTime />
-      <Cases/>
-      <Past14DaysChart/>
+      <div className="Home-container">
+        <div className="Home-main">
+          <DateTime />
+          <Cases />
+          <Past14DaysChart />
+        </div>
+        <div className="Home-side"></div>
+      </div>
     </div>
   );
 }
