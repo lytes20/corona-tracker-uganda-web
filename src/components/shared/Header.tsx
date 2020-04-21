29537b7522902aa9aa53928837563f7f5e4fd58e
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2020-04-21 04:50:00 +0300

    Add some responsiveness to header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
 import AcUnitIcon from '@material-ui/icons/AcUnit';
+import DehazeIcon from '@material-ui/icons/Dehaze';
 import IconButton from '@material-ui/core/IconButton';
 
 import '../../assets/styles/header.scss';
@@ -12,18 +13,47 @@ const menuItems = [
   { name: 'FAQs', path: '/corona-faqs' },
 ];
 
+function Menu(props) {
+  const { openMenu, toggleMenu } = props;
+  const menuClass = openMenu ? 'menu-container' : 'menu-container-none';
+  return (
+    <div className={menuClass}>
+      <div className="menu-list-container">
+        <ul>
+          <li>
+            <Link to="/" onClick={() => toggleMenu(false)}>
+              Home
+            </Link>
+          </li>
+          {menuItems.map((navLink) => (
+            <li key={navLink.name}>
+              <Link to={navLink.path} onClick={() => toggleMenu(false)}>
+                {navLink.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
 function Header(props) {
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleToggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
   return (
     <div className="Header-container">
       {/* Header item  */}
-      <div className="Header-item Header-logo-container" onClick={() => (window.location.href = '/')}>
+      <div className="Header-item Header-logo-container">
         {/* logo container */}
-        <div>
+        <div onClick={() => (window.location.href = '/')}>
           <IconButton>
             <AcUnitIcon />
           </IconButton>
         </div>
-        <div>
+        <div onClick={() => (window.location.href = '/')}>
           <h2>Corona Virus Tracker - Uganda</h2>
         </div>
       </div>
@@ -36,6 +66,12 @@ function Header(props) {
               <Link to={item.path}>{item.name}</Link>
             </div>
           ))}
+          <div className="burger">
+            <IconButton onClick={() => handleToggleMenu()}>
+              <DehazeIcon />
+            </IconButton>
+            <Menu openMenu={openMenu} toggleMenu={handleToggleMenu} />
+          </div>
         </div>
       </div>
     </div>
